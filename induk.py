2ffd2a6afedc9ebf943015a318798aab61caa085
MuhamadAndris <muhamad335andris@gmail.com>
2024-12-05 23:17:54 +0700

    tambahkan privet dan getter

--- induk.py
+++ induk.py
@@ -1,3 +1,15 @@
+from typing import Any
+
+# Class induk / Parent
 class Induk:
     def __init__(self):
-        self.aplication_name = "Aplikasi kasir"
+        self.__aplication_name = "Aplikasi kasir"
+        self.__screen_size = "1000x500"
+    
+    # Get Aplication name
+    def get_aplication_name(self):
+        return self.__aplication_name
+
+    # Get screen size
+    def get_screen_size(self):
+        return self.__screen_size
---
+++ product.py
@@ -0,0 +1,11 @@
+products = [
+    (1221, "Sepatu, 10000")
+]
+
+transakti = {
+    "id_transaksi" : 1210,
+    "pilihan_barnag" : [
+        [1221, "Sepatu, 10000"],
+        [1221, "Sepatu, 10000"],
+    ]
+}
