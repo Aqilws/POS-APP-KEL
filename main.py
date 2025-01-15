ed60f5637bda9244e4bc7661d7be26bbcd6aa6a1
Yoo <104003767+Aqilws@users.noreply.github.com>
2025-01-15 23:12:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 from transaksi import transaksi_menu
-from produk_barang import produk_barang_menu
-# from stok_barang import stok_barang_menu
+from stok_barang import produk
 from laporan_penjualan import Laporan_Penjualan
-from login import login
+from karyawan_profile import karyawanMain
+from login import login, logout
 
 def clear_screen():
     """Menghapus tampilan terminal."""
@@ -13,42 +13,44 @@ def show_menu():
     print("Daftar Menu Utama")
     print("[1] Transaksi")
     print("[2] Produk/Barang")
-    print("[3] Stok Barang")
-    print("[4] Laporan Penjualan")
+    print("[3] Laporan Penjualan")
+    print("[4] Profile")
+
     print("[0] Keluar")
 
 def main():
-    clear_screen()
-    logged_in_user = login()
-    
-    if logged_in_user is None:
-        print("Login gagal. Program berhenti.")
-        return
-        
     while True:
         clear_screen()
-        print(f"Selamat datang, {logged_in_user}!")
-        show_menu()
-        pilihan = input("\nPilih menu: ")
+        logged_in_user = login()
         
-        if pilihan == "1":
-            clear_screen()
-            transaksi_menu(logged_in_user)
-        elif pilihan == "2":
-            clear_screen()
-            produk_barang_menu()
-        elif pilihan == "3":
-            clear_screen()
-            stok_barang_menu()
-        elif pilihan == "4":
-            clear_screen()
-            Laporan_Penjualan().menu()
-        elif pilihan == "0":
-            clear_screen()
-            print("Keluar dari aplikasi. Terima kasih!")
+        if logged_in_user is None:
+            print("Login gagal. Program berhenti.")
             break
-        else:
-            print("Pilihan tidak valid, silakan coba lagi.")
+            
+        while True:
+            clear_screen()
+            print(f"Selamat datang, {logged_in_user}!")
+            show_menu()
+            pilihan = input("\nPilih menu: ")
+            
+            if pilihan == "1":
+                clear_screen()
+                transaksi_menu(logged_in_user)
+            elif pilihan == "2":
+                clear_screen()
+                produk()
+            elif pilihan == "3":
+                clear_screen()
+                Laporan_Penjualan().menu()
+            elif pilihan == "4":
+                clear_screen()
+                karyawanMain(logged_in_user)
+            elif pilihan == "0":
+                clear_screen()
+                logout()
+                break  
+            else:
+                print("Pilihan tidak valid, silakan coba lagi.")
 
 if __name__ == "__main__":
     main()
